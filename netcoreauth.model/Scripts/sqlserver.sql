3434100b9c82b81ee1f570c7389bcae590d54475
Wilson Wu <WilsonWu@Wilson-MBP.lan>
2017-06-11 01:12:43 +0800

    add scripts

--- netcoreauth.model/Scripts/sqlserver.sql
+++ netcoreauth.model/Scripts/sqlserver.sql
@@ -1,1 +1,14 @@
-﻿
+﻿CREATE TABLE [token] (
+    [Id] int IDENTITY(1,1) PRIMARY KEY,
+    [user_id] int NOT NULL,
+    [jwt_token] nvarchar(max) NOT NULL,
+    [token_type] nvarchar(max) NOT NULL
+);
+
+CREATE TABLE [user] (
+    [Id] int IDENTITY(1,1) PRIMARY KEY,
+    [email] nvarchar(max) NOT NULL,
+    [password] nvarchar(max) NOT NULL,
+    [is_activated] bit default 0 NOT NULL,
+    [is_disabled] bit default 0 NOT NULL
+);
