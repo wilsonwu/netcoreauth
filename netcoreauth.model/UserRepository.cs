632d436e091a18e87d755b8810b97fd841c0826b
Wilson Wu <iWilsonWu@hotmail.com>
2017-08-21 15:37:32 +0800

    clear and update packages

--- netcoreauth.model/UserRepository.cs
+++ netcoreauth.model/UserRepository.cs
@@ -1,12 +1,7 @@
 ﻿using Dapper;
-using System;
-using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
-using System.Security.Claims;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace netcoreauth.model
 {
