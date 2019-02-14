69038b3fb505cb68d080fae876125270ad2ee707
wilsonwu <iwilsonwu@gmail.com>
2019-02-14 15:43:08 +0800

    update to latest nuget packages

--- netcoreauth.api/Startup.cs
+++ netcoreauth.api/Startup.cs
@@ -77,9 +77,6 @@ namespace netcoreauth.api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
-
             app.UseCors("AllowAll");
 
             connString = ConfigurationExtensions.GetConnectionString(this.Configuration, "SQLSERVERDB");
